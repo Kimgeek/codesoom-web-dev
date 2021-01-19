539dadc02db2237cf263bd3abc6955e728bb08e5
Kimgeek <67546862+Kimgeek@users.noreply.github.com>
2021-01-19 15:06:04 +0900

    패키지, index.html, index.js

---
+++ src/index.js
@@ -0,0 +1,28 @@
+function createElement(tagName, ...children) {
+    const element = document.createElement(tagName);
+
+    children.forEach((child) => {
+    element.appendChild(child);
+    })
+
+    return element;
+}
+
+//
+
+//
+
+document.getElementById('app').appendChild(
+    createElement(
+        'div',
+        createElement(
+        'p',
+        document.createTextNode('Hello, world!'),
+        document.createTextNode('Hello, world!!!!!'),
+        ),
+        createElement(
+            'p',
+            document.createTextNode('Hi!'),
+        ),
+    ),
+);
